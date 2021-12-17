c4ceb773c045e8cf5c85225251fb558e5a2c9f65
Gerard Kasemba <gerardkasemba@gmail.com>
2021-12-17 13:11:59 -0500

    ecommerce

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('ecommerce', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+module.exports = sequelize;
